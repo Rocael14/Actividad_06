03b91032e5b19e41ac55f9f64a4e7ff070525339
Rocael14 <jc.vmayen@udeo.edu.gt>
2025-07-14 22:02:07 -0600

    Realizamos el case 2 para que ya muestre la lista de los productos

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ while True:
         match opcion:
             case 1:
                 print("\nIngresar Producto")
-                codigo_producto = input("Ingrese el codigo del producto:")
+                codigo_producto = input("Ingrese el codigo del producto: ")
                 if codigo_producto in productos:
                     print("El codigo ya existe")
                 else:
-                    nombre_producto = input("Ingrese el nombre del producto:")
-                    seleccion_categoria = int(input("Ingrese el categoria del producto (1. Hombre, 2.Mujer o 3.Niño)"))
+                    nombre_producto = input("Ingrese el nombre del producto: ")
+                    seleccion_categoria = int(input("Ingrese el categoria del producto (1. Hombre, 2.Mujer o 3.Niño): "))
                     if seleccion_categoria == 1:
                         categoria_producto = "Hombre"
                     elif seleccion_categoria == 2:
@@ -29,17 +29,28 @@ while True:
                         categoria_producto = "Niño"
                     else:
                         print("La categoria no existe")
-                        continue
-                    talla=input("Ingrese la Talla:")
-                    precio=int(input("Ingrese el precio unitario del producto:"))
+                    talla=input("Ingrese la Talla: ")
+                    precio=float(input("Ingrese el precio unitario del producto: "))
                     if precio>0:
-                        stock=int(input("Ingrese el stock del producto:"))
+                        stock=int(input("Ingrese el stock del producto: "))
                         if stock>0:
                             productos[codigo_producto]={"nombre_producto":nombre_producto, "categoria_producto":categoria_producto,
                                                         "talla":talla, "precio":precio, "stock":stock}
-                            print(f"Prodcuto {nombre_producto} ingresadp correctamente")
+                            print(f"Prodcuto {nombre_producto} ingresado correctamente")
+                        else:
+                            print("Stock debe ser mayor a 0")
+                    else:
+                        print("Precio ingresado debe ser mayor a 0")
             case 2:
-                print("Lista de Productos")
+                print("\nLista de Productos:")
+                for codigo_producto, producto in productos.items():
+                    print(f"\nCodigo del producto: {codigo_producto}")
+                    print(f"Nombre del producto: {producto['nombre_producto']}")
+                    print(f"Categoria del producto: {producto['categoria_producto']}")
+                    print(f"Talla: {producto['talla']}")
+                    print(f"Precio: {producto['precio']}")
+                    print(f"Stock: {producto['stock']}")
+                    print("----------------------------")
             case 3:
                 print("Buscar Producto")
             case 4:
